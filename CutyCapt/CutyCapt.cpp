e357697a9ee406e8578b2285b7ae95f5b788c37a
Björn Höhrmann <bjoern@hoehrmann.de>
2008-06-11 06:01:28 +0000

    Setting properties

--- CutyCapt/CutyCapt.cpp
+++ CutyCapt/CutyCapt.cpp
@@ -14,7 +14,7 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
-// $Id: CutyCapt.cpp,v 1.2 2008/06/07 22:12:06 hoehrmann Exp $
+// $Id$
 //
 ////////////////////////////////////////////////////////////////////
 
